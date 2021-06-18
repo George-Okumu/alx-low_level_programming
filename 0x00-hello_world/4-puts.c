29ea91c99c8aa76c021442585897ecd1e8b9d510
George Okumu <gokumu12@gmail.com>
2021-06-18 09:04:30 +0300

    removed lines at the start of a line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * function that prints out the string using the function puts
- *
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-    char my_string[] = "\"Programming is like building a multilingual puzzle";
-    puts(my_string);
+char my_string[] = "\"Programming is like building a multilingual puzzle";
+puts(my_string);
 
-    return (0);
+return (0);
 }
