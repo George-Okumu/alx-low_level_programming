af84118153fe4b09d9e75db0ef855f0d4f1af7b8
George Okumu <gokumu12@gmail.com>
2021-06-18 11:12:22 +0300

    removed printf method

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-int number = 1;
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(hello, sizeof(hello) - 1, 1, stdout);
 
-return (number);
+return (1);
 }
