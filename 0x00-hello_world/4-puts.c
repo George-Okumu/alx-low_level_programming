889923ad561883358aaac858326461c555e30b90
George Okumu <gokumu12@gmail.com>
2021-06-18 08:45:45 +0300

    added documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main()
+
+/**
+ * function that prints out the string using the function puts
+ *
+ * Return: 0
+ */
+int main(void)
 {
     char my_string[] = "\"Programming is like building a multilingual puzzle";
     puts(my_string);
