233943b7705b150e51cdc63691e26325056b6552
George Okumu <gokumu12@gmail.com>
2021-06-18 10:51:41 +0300

    printed out a string, returning one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints quote
+ * Description: Prints and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: 1
+ */
+int main(void)
+{
+int number = 1;
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (number);
+}
