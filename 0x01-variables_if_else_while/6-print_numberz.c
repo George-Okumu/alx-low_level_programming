b8350dee87e5b5c6c7f471cd38cd60912cae8856
George Okumu <gokumu12@gmail.com>
2021-06-20 09:58:30 +0300

    printed out numbers of base 10 with another format

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program program that
+ * Description: prints numbers of base 10
+ * not allowed to use any variable of type char
+ * Return: 0
+ */
+int main(void)
+{
+int base10;
+
+for (base10 = 0;base10 <= 9; base10++)
+putchar(base10);
+putchar('\n');
+
+return (0);
+}
