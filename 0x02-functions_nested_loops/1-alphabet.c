dd5854eb1f3f2d3be0e2f38c0fa353c7501ea1d9
George Okumu <gokumu12@gmail.com>
2021-06-22 16:56:45 +0300

    change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,10 @@
  *Description: in lowercase
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-
     int ch;
     for (ch = 'a'; ch <= 'z'; ch++)
     _putchar(ch);
-    _putchar('\n');   
-    return(0);
+    _putchar('\n');
 }
