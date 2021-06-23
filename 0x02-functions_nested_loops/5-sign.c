4cad990038f79a7c231505cbcf9b0e6df8bcfb6a
George Okumu <gokumu12@gmail.com>
2021-06-23 16:24:59 +0300

    return 1  if and printed + when n is positive and viceversa

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+/**
+ * print_sign - prints the sign of a number
+ * @n: integer to be tested
+ * Return: 1 and prints + if n is greater than zero
+ * returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return ('+');
+}
+else
+{
+return ('-');
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -16,4 +16,7 @@ int _islower(int c);
 /*checks for alphabetic character*/
 int _isalpha(int c);
 
+/*prints sign of a number*/
+int print_sign(int n);
+
 #endif
