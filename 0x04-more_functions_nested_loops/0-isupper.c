3ca01a10962a3f36d119c170943f8b6e6d2ba1d1
George Okumu <gokumu12@gmail.com>
2021-06-24 17:07:22 +0300

    prototype that checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include  "holberton.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: returns 1 if c is integer
+ * Return: 1/0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+_putchar('1');
+}
+else
+{
+_putchar('0');
+}
+
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTO_H
+#define HOLBERTON_H
+
+/*for checking uppercase*/
+int _isupper(int c);
+
+#endif
