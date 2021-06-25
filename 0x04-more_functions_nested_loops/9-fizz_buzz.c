60a5a55afd0ca4ea0197d7d5c1d918420ae1e273
George Okumu <gokumu12@gmail.com>
2021-06-25 23:12:53 +0300

    printed fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print fizbuzz
+ * Description: prints the numbers from 1 to 100, followed by a new line.
+ * for multiples of three print Fizz instead of the number
+ * for the multiples of five print Buzz.
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3) == 0)
+{
+printf("fizz ");
+
+}
+else if ((i % 5) == 0)
+{
+
+printf("buzz ");
+}
+
+else
+{
+printf("%d ", i);
+}
+
+}
+printf("\n");
+return (0);
+}
