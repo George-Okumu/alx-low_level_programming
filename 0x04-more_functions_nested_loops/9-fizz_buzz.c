b5cf46901da29ca6ad790431d9a87f39d64d02fa
George Okumu <gokumu12@gmail.com>
2021-06-25 23:30:45 +0300

    change

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,19 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3) == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("fizz ");
+printf("fizzbuzz ");
 
 }
 else if ((i % 5) == 0)
 {
 
 printf("buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("fizz ");
 }
 
 else
