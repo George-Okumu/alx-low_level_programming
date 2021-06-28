ed698ba1aa82c272b107de6ed253991f13150232
George Okumu <gokumu12@gmail.com>
2021-06-28 18:55:23 +0300

    function to update the value the parameter points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - takes pointer
+ * @n: updates the value it points to to 98.
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+int number = 98;
+*n = number;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Prints holberton*/
+int _putchar(char);
+
+/* updates the value it points to to */
+void reset_to_98(int *n);
+
+#endif
