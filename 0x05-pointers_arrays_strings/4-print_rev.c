4f4233f36434d8c24925af0a3a2c1919d5898cda
George Okumu <gokumu12@gmail.com>
2021-06-29 12:32:06 +0300

    change

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,45 +2,26 @@
 #include <stdio.h>
 /**
  * print_rev - prints string in reverse
- * _strlen - prints the length of a string
- * @c: pointer to character
- * @s: the pointer to the string
+ * @s: string to be reversed
  */
-int _strlen(char *c)
-{
-int ch = 0;
-while (c[ch])
-ch++;
-return (ch);
-}
+
 
 void   print_rev(char *s)
 {
-int i, m;
-char *end_pointer, *beginner_pointer, ch, t;
-/*Getting the length of the string*/
-m = _strlen(s);
+int i = 0;
 
-/*setting the pointers to start of string(s)*/
-end_pointer = s;
-beginner_pointer = s;
-
-/*move the end pointer to the last char*/
-for (i = 0; i < (m - 1); i++)
-end_pointer++;
-
-/*swap the char from start to end*/
-for (i = 0; i < (m / 2); i++)
+while (*(s + i) != '\0')
 {
-/*swap character*/
-ch = *end_pointer;
-t = *beginner_pointer;
-*beginner_pointer = ch;
-*end_pointer = t;
+i++;
+}
 
-/*update pointers positions*/
-beginner_pointer++;
-end_pointer--;
+i--;
+while (i >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
+_putchar('\n');
 
 }
-}
+
