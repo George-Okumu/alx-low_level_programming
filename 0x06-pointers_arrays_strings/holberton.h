ac1345caba9d1efa4949e4cf27c670b0969c2811
George Okumu <gokumu12@gmail.com>
2021-07-01 16:26:16 +0300

    function that concatinates string with n bytes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,4 +4,7 @@
 /*concantenates two strings*/
 char *_strcat(char *dest, char *src);
 
+/*concantecates two strings but uses n bytes from src*/
+char *_strncat(char *dest, char *src, int n);
+
 #endif
