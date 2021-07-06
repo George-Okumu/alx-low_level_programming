f5da9b3829bd6f07b3333813a40268d345b75da8
George Okumu <gokumu12@gmail.com>
2021-07-06 08:58:20 +0300

    function that locates a substring

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -16,4 +16,7 @@ unsigned int _strspn(char *s, char *accept);
 /*searches a string for any of a set of bytes*/
 char *_strpbrk(char *s, char *accept);
 
+/*locates a substring*/
+char *_strstr(char *haystack, char *needle);
+
 #endif
