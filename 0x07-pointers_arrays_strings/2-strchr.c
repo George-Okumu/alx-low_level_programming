92e63119d8e0fcbb9b1c45c02421e43bb90ae43f
George Okumu <gokumu12@gmail.com>
2021-07-06 06:44:49 +0300

    a function that locates a characyer in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string pointer
+ * @c: character located
+ * Return: charcter to the first occurence of c in string s
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (c == *s)
+{
+return (s);
+}
+s++;
+}
+
+return (NULL);
+}
