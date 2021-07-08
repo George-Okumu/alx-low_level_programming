5031623533aabc70f6cfa5e9726cf69fe57ebcfc
George Okumu <gokumu12@gmail.com>
2021-07-08 14:41:21 +0300

    added a function to print string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints string
+ * @s: string holder
+ */
+void _puts_recursion(char *s)
+{
+
+while (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(++s);
+}
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Prints holberton*/
+int _putchar(char);
+
+/* prints a string, followed by a new line.*/
+void _puts_recursion(char *s);
+
+#endif
