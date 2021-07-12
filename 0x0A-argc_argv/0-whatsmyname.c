be45a592ffa4ecdeda9dcf71625f566326945eea
George Okumu <gokumu12@gmail.com>
2021-07-12 16:37:58 +0300

    function that prints argv at 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ * Return: true
+ */
+int main(int __attribute__((__unused__))  argc,  char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
