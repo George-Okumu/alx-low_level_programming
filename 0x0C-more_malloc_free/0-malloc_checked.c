15738c30e8869931b58542a9114facd750430478
George Okumu <gokumu12@gmail.com>
2021-07-17 17:17:39 +0300

    function to allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * malloc_checked - allocate memory
+ * @b: int to be allocated memory
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+return (malloc(b));
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*Allocates memory using malloc */
+void *malloc_checked(unsigned int b);
+
+#endif
