4ef8f938d765a5efae036d8b6d9bdebd6641fb40
George Okumu <gokumu12@gmail.com>
2021-07-29 21:08:49 +0300

    made betty happy

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * 
- * 
- * 
- * 
+ * print_list - print all elements of list_t
+ * @h: nodes
+ * Return: no. of nodes
  */
 size_t print_list(const list_t *h)
 {
 
 size_t ge_nodes = 0;
-const list_t *num__of_nodes= h;
+const list_t *num__of_nodes = h;
 
 while (num__of_nodes != NULL)
 {
