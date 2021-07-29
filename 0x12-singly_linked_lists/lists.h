e8867bbc306fac479b1f465e3966b21835972a74
George Okumu <gokumu12@gmail.com>
2021-07-29 21:24:18 +0300

    add new node at the end of list_t

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,6 +27,9 @@ size_t list_len(const list_t *h);
 /*Adds new node at the begging of a lsit*/
 list_t *add_node(list_t **head, const char *str);
 
+/*Adds new node at the end of a list_t*/
+list_t *add_node_end(list_t **head, const char *str);
+
 /*put char*/
 int _putchar(char c);
 
